12fd09344dd6783db381df1ba5bcb56f9c8b2df6
Justin Coyne <jcoyne@justincoyne.com>
2018-08-01 16:41:00 -0500

    Link to the reports for each department

--- app/javascript/blacklight/layouts/reports.vue
+++ app/javascript/blacklight/layouts/reports.vue
@@ -1,8 +1,13 @@
 <template>
   <section class="container">
     <h1>Reports</h1>
-    <ul>
-      <li><a href="/reports/3.csv">Collaboration Report</a></li>
+    <label for="department">Department</label>
+    <select name="department" v-model="selectedDepartment">
+      <option v-for="department in departments" :value="department">{{ department.label }}</option>
+    </select>
+
+    <ul v-if="selectedDepartment !== ''">
+      <li><a href="#" v-on:click="download">Collaboration Report</a></li>
     </ul>
   </section>
 </template>
@@ -10,6 +15,24 @@
 
 <script>
 export default {
+  data: function () {
+    return {
+      selectedDepartment: '',
+      departments: [],
+    }
+  },
+  created() {
+    var result = this.$http.get('/departments').then(function(response){
+        this.departments = response.data
+    }, function(error){
+        console.error(error.statusText);
+    })
+  },
+  methods: {
+    download: function() {
+      window.location = `/reports/${this.selectedDepartment.id}.csv`
+    }
+  }
 }
 </script>
 
