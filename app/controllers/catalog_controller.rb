31e3399758bbdb8626e404fcf5ffb4ac5c6573a6
Justin Littman <justinlittman@stanford.edu>
2018-10-30 12:48:53 -0400

    Adds institute facet. Cleans up facet config.
    closes #129

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -78,11 +78,9 @@ class CatalogController < ApplicationController
     config.add_facet_field 'type_ssi', label: 'Type'
     config.add_facet_field 'pub_year_ssim', label: 'Publication Year', single: true, limit: 1000
     config.add_facet_field 'subject_label_ssim', label: 'Topic', limit: 1000, index_range: 'A'..'Z'
-    config.add_facet_field 'institute_ssim', label: 'Institute', limit: 1000
     config.add_facet_field 'school_label_ssim', label: 'School', limit: 1000
     config.add_facet_field 'department_label_ssim', label: 'Department', limit: 1000
-    config.add_facet_field 'subdivision_ssim', label: 'Subdivision', limit: 1000
-    config.add_facet_field 'division_ssim', label: 'Division', limit: 1000
+    config.add_facet_field 'institute_label_ssim', label: 'Institute', limit: 1000
     config.add_facet_field 'institution_label_ssim', label: 'Institution', limit: 1000
     config.add_facet_field 'agent_ssim', label: 'Agent'
     config.add_facet_field 'concept_labels_ssim', label: 'Concept', limit: 1000
@@ -106,6 +104,7 @@ class CatalogController < ApplicationController
       config.add_index_field 'name_tsim', label: 'Name'
       config.add_index_field 'department_label_ssim', label: 'Department'
       config.add_index_field 'school_label_ssim', label: 'School'
+      config.add_index_field 'institute_label_ssim', label: 'Institute'
       config.add_index_field 'institution_label_ssim', label: 'University'
     end
 
