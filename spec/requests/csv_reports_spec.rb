db0bd9ecd14eb33e03b06a80c4351c6786521eda
Justin Littman <justinlittman@stanford.edu>
2018-11-01 12:33:50 -0400

    Switched chloropleth to use AuthorsCoauthorCountriesReportGenerator.
    closes #144

--- spec/requests/csv_reports_spec.rb
+++ spec/requests/csv_reports_spec.rb
@@ -20,18 +20,4 @@ RSpec.describe 'CSV reports', type: :request do
         .with(ActionController::Parameters.new(department_uri: '7'))
     end
   end
-
-  context 'when the choropleth report is requested' do
-    before do
-      allow(ChoroplethReportGenerator).to receive(:generate).and_return(csv)
-    end
-
-    it 'returns a report for driving the choropleth visualization' do
-      get '/reports/choropleth.csv?department_uri=7'
-      expect(response.body).to eq "Foo,Bar\n"
-      expect(response.content_type).to eq 'text/csv'
-      expect(ChoroplethReportGenerator).to have_received(:generate)
-        .with(ActionController::Parameters.new(department_uri: '7'))
-    end
-  end
 end
