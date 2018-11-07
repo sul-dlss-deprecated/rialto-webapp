1e94e622be53e5d3c4d4d4e724f2248de2159ed6
Justin Coyne <jcoyne@justincoyne.com>
2018-11-07 12:42:21 -0600

    Add a start over button

---
+++ app/javascript/blacklight/search/startOver.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="constraints-container" v-if="showButton" >
+    <router-link :to="{ name: 'search'}" class="btn btn-primary">Start Over</router-link>
+  </div>
+</template>
+
+<script>
+
+export default {
+  computed: {
+    showButton() {
+      return typeof(this.$route.params.filter) !== 'undefined'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
