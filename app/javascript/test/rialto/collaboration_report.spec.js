c33af1944839a7493fbc5022791018a5a8def436
Justin Littman <justinlittman@stanford.edu>
2018-11-13 17:08:36 -0800

    Switched default collab report to by country.
    closes #210.

--- app/javascript/test/rialto/collaboration_report.spec.js
+++ app/javascript/test/rialto/collaboration_report.spec.js
@@ -21,7 +21,7 @@ describe('CollaborationReport', () => {
     it("is a url when the department is selected", () => {
       var currentYear = (new Date()).getFullYear()
       vm.selectedDepartment = { uri: "123" }
-      expect(vm.reportURL).to.equal(`/reports/coauthors.csv?org_uri=123&start_year=2000&end_year=${currentYear}`)
+      expect(vm.reportURL).to.equal(`/reports/coauthor-countries.csv?org_uri=123&start_year=2000&end_year=${currentYear}`)
     })
   })
 })
